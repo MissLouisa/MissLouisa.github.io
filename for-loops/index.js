9e30b114b9f2194d8820ab7bff0f4971309437d1
= <=>
2019-11-10 15:09:52 -0800

    update javascript stuff

---
+++ for-loops/index.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    const elfCode = {
+        appendToList: (list, value) => {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(value));
+            list.
+        }
+}
+    const listButton = document.getElementById('runForLoop');
+
+    listButton.onclick = () => {
+        const myList = document.getElementById('myList');
+
+        elfCode.appendToList(myList, i);
+
+    }
+}
---
+++ javascript-tests/primitives.js
@@ -0,0 +1,23 @@
+const myString = "Learning JavaScript in class";
+const myFirstNumericVariable = 3;
+const myBoolean = true;
+const myNull = null;
+const constantVariable = "This variable can not be change. Except...";
+const quotable = 'He said, "Hello". That\'s all folks.';
+
+var oldStyle = "This is the way we used to do it";
+
+let newChangableStyly = "This allows you to declare a variable that can be changed";
+let myUndefined;
+
+console.log(myString);
+console.log(typeof myString);
+console.log(myFirstNumericVariable);
+console.log('TYPE:', typeof myFirstNumericVariable);
+console.log(myBoolean);
+console.log('TYPE:', typeof myBoolean);
+console.log(myNull);
+console.log('TYPE:', typeof myNull);
+console.log(myUndefined);
+console.log('TYPE:', typeof myUndefined);
+console.log('Add 3 + 25: ', myFirstNumericVariable + 25);
