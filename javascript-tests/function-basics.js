e6b5380c4b6caadd2924391d97e998e808fd985d
= <=>
2019-11-10 20:44:37 -0800

    update system

---
+++ javascript-tests/function-basics.js
@@ -0,0 +1,12 @@
+function simpleFunction() {
+    console.log("funções devem consistir em instruções projetadas para executar uma única tarefa.");
+}
+
+const parameter = 25;
+function functionParameters(parameter) {
+    console.log(parameter);
+}
+
+function functionReturn() {
+    return console.log('Many functions return values.');
+}
