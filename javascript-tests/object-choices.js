d326c62e0318a3d637cfd77bd27cdeaec2fb475c
= <=>
2019-11-24 22:01:20 -0800

    update object choices

--- javascript-tests/object-choices.js
+++ javascript-tests/object-choices.js
@@ -1,43 +1,54 @@
 window.onload = () => {
 
-    const simpleObjectDisplay = document.getElementById('simpleObjectDisplay');
-    const simpleObjectDisplay = document.getElementById('simpleObjectAction');
+    const simpleDisplay = document.getElementById("simpleObjectDisplay")
+    const simpleAction = document.getElementById("simpleObjectAction")
 
-    simpleObjectAction
-const simpleObject = {
-    firstName: 'James',
-    lastName: 'Watson',
-    sayName: function () {
-        const fullName = this.firstName + " " + this.lastName;
-        console.log(this.firstName + " " + this.lastName);
-        simpleObjectDisplay.textContent = fullName;
-    }
-};
+    const simpleObject = {
+        firstname: "James",
+        lastname: "Jackson",
+        sayName: function () {
+            return this.firstname + " " + this.lastname ;
+        }
+    };
 
-simpleObjectAction.onclick = function() {
+    simpleAction.onclick = () => {
+        simpleDisplay.textContent = simpleObject.sayName();
+    };
+
     simpleObject.sayName();
-}
 
-simpleObject.dynamicMethod = () => {
-    console.log("Dynamic Method");
-}
-
-funtion Foo() {
-    FunctionObject.prototype.sayName = function() {
-        console.log('Function Object');
+    simpleObject.dynamic = function () {
+        console.log("dynamic method.");
     }
+
+    simpleObject.dynamic();
+
+    // function constructor
+    function FunctionObject() {
+        FunctionObject.prototype.proto = function () {
+            return 'Function object';
+        }
+    };
+
+    FunctionObject.prototype.dynamicProto = function () {
+        return 'dynamic proto';
+    };
+
+    let obj = new FunctionObject();
+    console.log(obj.proto());
+    console.log(obj.dynamicProto());
+    console.log(obj.constructor.toString());
+
+    class print {
+        constructor(word) {
+            this.word = word;
+        }
+
+        printMe() {
+            console.log(this.word);
+        }
+    };
+
+    let myWord = new print("my word");
+    myWord.printMe();
 }
-
-class CostumClass = {
-    sayName() {
-        console.log("CustomClass");
-    }
-}
-simpleObject.sayName();
-simpleObject.dynamicMethod();
-
-const functionObject = new FunctionObject();
-functionObject.sayName();
-
-const CustomClass = new CustomClass();
-CustomClass.sayName();
