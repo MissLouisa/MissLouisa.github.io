38ed09a9cdd6e5f925ceb6eeb79d6274d6cb477e
= <=>
2019-12-05 08:20:41 -0800

    site mobile radio choice donee

---
+++ javascript-tests/dark-mode.js
@@ -0,0 +1,33 @@
+window.onload = () => {
+  const switchStylesButton = document.getElementById("switchStylesAction");
+  const styleForm = document.getElementById("change-style");
+  const defaults = document.getElementById("default");
+  const alternate = document.getElementById("alternate");
+  const regularButtons = document.getElementById("buttons1");
+  const switchButtons = document.getElementById("buttons2");
+
+  styleForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const value = document.querySelector('input[name="switcher"]:checked')
+      .value;
+    if (value.toLowerCase() === "alternate") {
+      disableStylesheet(defaults);
+      enableStylesheet(alternate);
+      disableStylesheet(regularButtons);
+      enableStylesheet(switchButtons);
+    } else {
+      disableStylesheet(alternate);
+      enableStylesheet(defaults);
+      disableStylesheet(switchButtons);
+      enableStylesheet(regularButtons);
+    }
+  });
+};
+
+function enableStylesheet(node) {
+  node.rel = "stylesheet";
+}
+
+function disableStylesheet(node) {
+  node.rel = "alternate stylesheet";
+}
