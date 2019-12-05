4c9d0e4f95e0fc3dc287e5888ed8bbb7fa8d5f46
= <=>
2019-12-05 10:52:21 -0800

    finished

--- javascript-tests/dark-mode.js
+++ javascript-tests/dark-mode.js
@@ -1,25 +1,18 @@
 window.onload = () => {
   const switchStylesButton = document.getElementById("switchStylesAction");
-  const styleForm = document.getElementById("change-style");
+  const styleForm = document.getElementById("option-template");
   const defaults = document.getElementById("default");
   const alternate = document.getElementById("alternate");
-  const regularButtons = document.getElementById("buttons1");
-  const switchButtons = document.getElementById("buttons2");
 
   styleForm.addEventListener("submit", event => {
     event.preventDefault();
-    const value = document.querySelector('input[name="switcher"]:checked')
-      .value;
-    if (value.toLowerCase() === "alternate") {
-      disableStylesheet(defaults);
-      enableStylesheet(alternate);
-      disableStylesheet(regularButtons);
-      enableStylesheet(switchButtons);
-    } else {
+    const value = document.querySelector('input[name="switcher"]:checked').value;
+    if (value.toLowerCase() === "default") {
       disableStylesheet(alternate);
       enableStylesheet(defaults);
-      disableStylesheet(switchButtons);
-      enableStylesheet(regularButtons);
+    } else {
+      disableStylesheet(defaults);
+      enableStylesheet(alternate);
     }
   });
 };
