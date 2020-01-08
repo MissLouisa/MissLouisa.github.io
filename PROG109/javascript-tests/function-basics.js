49a773c536fd5b3945fc0c4b173ad5249beb144c
MissLouisa <kurtfriedrich@hotmail.com>
2020-01-07 16:28:38 -0800

    folder to prog109

---
+++ PROG109/javascript-tests/function-basics.js
@@ -0,0 +1,49 @@
+const parameters = "Many functions take parameters.";
+
+function simpleFunction() {
+    const displayText = "Functions should consist of statements designed to perform a single task.";
+    console.log(displayText);
+
+    simpleFunctionDisplay.textContent = displayText;
+}
+
+function functionParameters(parameters) {
+    console.log(parameters);
+
+    functionParametersDisplay.textContent = parameters;
+}
+
+function functionReturn() {
+    const displayReturn = "Many functions return values.";
+
+    functionReturnDisplay.textContent = displayReturn;
+    return displayReturn;
+}
+
+window.onload = () => {
+    const simpleFunctionDisplay = document.getElementById("simpleFunctionDisplay");
+    const functionParametersDisplay = document.getElementById("functionParametersDisplay");
+    const functionReturnDisplay = document.getElementById("functionReturnDisplay");
+    const functionCallsAction = document.getElementById("functionCallsAction");
+    const listButton = document.getElementById("addToListAction");
+
+    functionCallsAction.onclick = function() {
+        // Both these functions get their display control
+        simpleFunction();
+        functionParameters(parameters);
+        functionReturn();
+    };
+
+    listButton.onclick = () => {
+        const newList = document.getElementById("listDisplay");
+        addToList.appendToList(newList, nameDisplay.value);
+    };
+
+    const addToList = {
+        appendToList: (list, value) => {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(value));
+            list.appendChild(li);
+        }
+    };
+};
