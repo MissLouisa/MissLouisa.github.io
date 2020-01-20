7ac36b08b64674be2a659dadc6adaffde4a5b5b4
MissLouisa <m.cartaxo@bellevuecollge.edu>
2020-01-20 13:16:24 -0800

    i did it

--- PROG209/javascript/swapletters.js
+++ PROG209/javascript/swapletters.js
@@ -1,17 +1,58 @@
-let words = {};
-let wordstoswap = document.querySelector("words");
-let swap;
-let word = prompt("Please enter a word", "");
-function wordstoswap() {
-    for (let i = 0; i < 3; i++) {
-        if (word != null) {
-            document.getElementById("input").textContent = word[i];
-        }
-    }
+///necessary to swap the letters
+function reverse_str(s) {
+    let str = s.split("");
+    let rs = str.reverse();
+    return rs.join("");
 }
 
-for (int j = 0; j < 3; j++) {
-    for (int k = 3; k >= 0; k--) {
-        eval(words.toString().replace(words[i], words[k])); 
-    } 
+let arrayWords = [];
+
+//function that gets the words from user
+function getWord() {
+    arrayWords = [];
+    for (let i = 0; i < 3; i++) {
+        //popup
+        let wordByUser = prompt("Please enter a word", "type anything");
+        //array didnt accept null and if the user cancel, the arrays is going to be reseted
+        if (wordByUser == null) {
+            arrayWords.splice(0,2);
+            break;
+        }
+        arrayWords.push(wordByUser);
+    }
+    let u = document.getElementById('listDisplay');
+
+    //create li tag with the words from user
+    for (let i = 0; i < arrayWords.length; i++) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(arrayWords[i]));
+        u.appendChild(li);
+    }
+
+    //first button disappears
+    document.getElementById('wordByUser').style.visibility = 'hidden';
+
+    //the second button is false if visible
+    document.getElementById('toggler').style.visibility = 'visible';
+}
+
+//function about the second button when clicked
+function offline() {
+
+    document.getElementById('toggler').style.visibility = 'hidden';
+
+    let v = document.getElementById('listDisplay');
+
+    //swap 
+    while (v.firstChild) {
+        v.firstChild.remove();
+    }
+
+    //new li, it is with the words swapped
+    for (let j = 0; j < arrayWords.length; j++) {
+        let li = document.createElement('li');
+        let newword = reverse_str(arrayWords[j])
+        li.appendChild(document.createTextNode(newword));
+        v.appendChild(li);
+    }
 }
