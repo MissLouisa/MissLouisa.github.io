0643bbca041793d518b65a23cf875d85ec514e09
MissLouisa <m.cartaxo@bellevuecollge.edu>
2020-01-19 18:02:36 -0800

    doneee

--- PROG209/javascript/swapletters.js
+++ PROG209/javascript/swapletters.js
@@ -1,11 +1,17 @@
 let words = {};
-
-for (int i = 0; i < 3; i++) {
-    prompt("Please enter a word", i); 
-    document.getElementById("words");
-
+let wordstoswap = document.querySelector("words");
+let swap;
+let word = prompt("Please enter a word", "");
+function wordstoswap() {
+    for (let i = 0; i < 3; i++) {
+        if (word != null) {
+            document.getElementById("input").textContent = word[i];
+        }
+    }
 }
 
-window.onload = () => {
-
+for (int j = 0; j < 3; j++) {
+    for (int k = 3; k >= 0; k--) {
+        eval(words.toString().replace(words[i], words[k])); 
+    } 
 }
